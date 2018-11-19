fe9a10c9d9ddeb72f4c60cff33e469c8acfc6fc4
António Matos <antonio.amdpaula@gmail.com>
2018-11-19 15:41:33 +0000

    Just renamed main function

--- main.py
+++ main.py
@@ -5,14 +5,19 @@ class Problem(csp.CSP):
 
     def __init__(self, fh):
 
+        #TODO Write function that from filestream fh, sets variables, domains, graph, and constraints_function
+
         super().__init__(variables, domains, graph, constraints_function)
 
     def dump_solution(self, fh):
 
 
-# Place here your code to write solution to opened file object fh
+        #TODO  Place here your code to write solution to opened file object fh
 
 def solve(input_file, output_file):
     p = Problem(input_file)
     # Place here your code that calls function csp.backtracking_search(self, ...)
     p.dump_solution(output_file)
+
+
+
